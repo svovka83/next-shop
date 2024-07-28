8d2e8f78c86ff9f587e01902a322cd80f806eabe
Volodymyr Smoliak <svovka83@gmail.com>
2024-07-29 01:12:05 +0300

    create seed

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -9,6 +9,8 @@ export async function GET() {
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
+
   const user = await prisma.user.create({ data: body });
+
   return NextResponse.json(user);
 }
