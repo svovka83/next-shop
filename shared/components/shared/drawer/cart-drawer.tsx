76b771fecd70cad3a4bf4b83c5a966aa7567def7
Volodymyr Smoliak <svovka83@gmail.com>
2024-08-12 21:29:27 +0300

    empty-box

--- shared/components/shared/drawer/cart-drawer.tsx
+++ shared/components/shared/drawer/cart-drawer.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetFooter,
   SheetHeader,
@@ -11,8 +13,8 @@ import {
 } from "@/shared/components/ui/sheet";
 import Link from "next/link";
 import { Button } from "../../ui";
-import { ArrowRight } from "lucide-react";
-import { CartDrawerItem } from "..";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { CartDrawerItem, Title } from "..";
 import { getCartItemInfo } from "../../../functions";
 import { useCartStore } from "@/shared/store";
 import { PizzaSizes, PizzaTypes } from "@/shared/constants/pizzaTypes";
@@ -58,51 +60,83 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({
       <SheetTrigger asChild>{children}</SheetTrigger>
 
       <SheetContent className="flex flex-col justify-between pb-0 bg-[#fff0f0]">
-        <SheetHeader>
-          <SheetTitle>
-            <span>In basket {cartItems.length} goods</span>
-          </SheetTitle>
-        </SheetHeader>
+        {totalAmount > 0 && (
+          <SheetHeader>
+            <SheetTitle>
+              <span>In basket {cartItems.length} goods</span>
+            </SheetTitle>
+          </SheetHeader>
+        )}
 
-        <div className="-mx-6 flex-1 overflow-auto">
-          {cartItems.map((item) => (
-            <div className="mb-2" key={item.id}>
-              <CartDrawerItem
-                id={item.id}
-                imageUrl={item.imageUrl}
-                details={getCartItemInfo(
-                  item.size as PizzaSizes,
-                  item.type as PizzaTypes,
-                  item.ingredients
-                )}
-                name={item.name}
-                price={item.price}
-                quantity={item.quantity}
-                onClickCountButton={(type) =>
-                  onClickCountButton(item.id, item.quantity, type)
-                }
-                onClickRemove={() => removeCartItem(item.id)}
-                disabled={item.disabled}
-              />
-            </div>
-          ))}
-        </div>
-        <SheetFooter className="-mx-6 p-8 bg-[#c9c9c9]">
-          <div className="w-full">
-            <div className="flex mb-4">
-              <span className="flex flex-1 text-lg text-neutral-600">
-                Total
-              </span>
-              <span className="font-bold text-lg">{totalAmount} $</span>
-            </div>
-            <Link href="/cart">
-              <Button type="submit" className="w-full h-12 text-base">
-                <b>Make order</b>
-                <ArrowRight className="w-5 ml-2" />
+        {!totalAmount && (
+          <div className="flex flex-col items-center justify-center w-72 m-auto">
+            <Image
+              src="/assets/images/empty-box.png"
+              alt="empty-cart"
+              width={120}
+              height={120}
+            />
+            <Title
+              text="Cart is empty"
+              size="lg"
+              className="text-center font-bold mt-2"
+            />
+            <p className="text-center text-neutral-500 mb-2">
+              Add something to cart for make your order, please
+            </p>
+            <SheetClose>
+              <Button className="w-56 h-12 text-base group" size="lg">
+                <ArrowLeft className="w-8 h-5 group-hover:-translate-x-1 transition" />
+                Back
               </Button>
-            </Link>
+            </SheetClose>
           </div>
-        </SheetFooter>
+        )}
+
+        {totalAmount > 0 && (
+          <>
+            <div className="-mx-6 flex-1 overflow-auto">
+              {cartItems.map((item) => (
+                <div className="mb-2" key={item.id}>
+                  <CartDrawerItem
+                    id={item.id}
+                    imageUrl={item.imageUrl}
+                    details={getCartItemInfo(
+                      item.size as PizzaSizes,
+                      item.type as PizzaTypes,
+                      item.ingredients
+                    )}
+                    name={item.name}
+                    price={item.price}
+                    quantity={item.quantity}
+                    onClickCountButton={(type) =>
+                      onClickCountButton(item.id, item.quantity, type)
+                    }
+                    onClickRemove={() => removeCartItem(item.id)}
+                    disabled={item.disabled}
+                  />
+                </div>
+              ))}
+            </div>
+
+            <SheetFooter className="-mx-6 p-8 bg-[#c9c9c9]">
+              <div className="w-full">
+                <div className="flex mb-4">
+                  <span className="flex flex-1 text-lg text-neutral-600">
+                    Total
+                  </span>
+                  <span className="font-bold text-lg">{totalAmount} $</span>
+                </div>
+                <Link href="/cart">
+                  <Button type="submit" className="w-full h-12 text-base">
+                    <b>Make order</b>
+                    <ArrowRight className="w-5 ml-2" />
+                  </Button>
+                </Link>
+              </div>
+            </SheetFooter>
+          </>
+        )}
       </SheetContent>
     </Sheet>
   );
